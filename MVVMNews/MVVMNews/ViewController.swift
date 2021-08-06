9a3895e7ced70d1f5da40702290b4645ed89ed57
so yeon <alicesysy@swu.ac.kr>
2021-08-06 10:56:57 +0900

    [add] MVVM Sample 프로젝트 생성

---
+++ MVVMNews/MVVMNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMNews
+//
+//  Created by soyeon on 2021/08/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
