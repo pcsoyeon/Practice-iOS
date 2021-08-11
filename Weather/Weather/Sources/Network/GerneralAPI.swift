ef0907904bec49d0cf432e26602ffd412590d620
so yeon <alicesysy@swu.ac.kr>
2021-08-12 01:19:55 +0900

    [chore] 폴더링 수정

---
+++ Weather/Weather/Sources/Network/GerneralAPI.swift
@@ -0,0 +1,13 @@
+//
+//  GerneralAPI.swift
+//  Weather
+//
+//  Created by soyeon on 2021/08/09.
+//
+
+import Foundation
+
+struct GeneralAPI {
+    static let baseURL = "https://api.weatherbit.io/v2.0"
+    static let key = "66c89713f9c747a6a71abecca4b31765"
+}
