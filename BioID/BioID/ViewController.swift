97a438b9c8e28c8874310759b507e0f4d9ede178
so yeon <alicesysy@swu.ac.kr>
2021-08-20 00:35:15 +0900

    [feat] tocuhID 구현

--- BioID/BioID/ViewController.swift
+++ BioID/BioID/ViewController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    private var touchIDButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("TRY TOUCH ID", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+    
+    // MARK: - Local Variables
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configUI()
+        setAction()
     }
+}
 
-
+extension ViewController {
+    func configUI() {
+        view.addSubview(touchIDButton)
+        touchIDButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        touchIDButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        touchIDButton.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func setAction() {
+        let touchIDAction = UIAction { _ in
+            print("!!!")
+            let context: LAContext = LAContext()
+            
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) {
+                context.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: "지문 내놔") { wasSuccessful, error in
+                    if wasSuccessful {
+                        print("WAS A SUCCESSFUL")
+                    } else {
+                        print("NOT LOGGED IN")
+                    }
+                }
+            }
+        }
+        touchIDButton.addAction(touchIDAction, for: .touchUpInside)
+    }
+    
+    
 }
 
