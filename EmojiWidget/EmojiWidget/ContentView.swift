875adca7f0460f8fa42162ba6245c4b49f53453c
so yeon <alicesysy@swu.ac.kr>
2021-08-20 13:35:28 +0900

    [add] emoji widget 프로젝트 추가

---
+++ EmojiWidget/EmojiWidget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmojiWidget
+//
+//  Created by soyeon on 2021/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
