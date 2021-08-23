0265c066702d00c067ad79e5cfe8365cd4bebc7a
so yeon <alicesysy@swu.ac.kr>
2021-08-23 17:33:56 +0900

    [feat] Shake Gesture 구현

--- ShakeGesture/ShakeGesture/ViewController.swift
+++ ShakeGesture/ShakeGesture/ViewController.swift
@@ -11,9 +11,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .gray
+        becomeFirstResponder()
     }
 
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+//            let alert = UIAlertController(title: "Detected Shake", message: "User got mad and shook their phone.", preferredStyle: .alert)
+//            alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+//            present(alert, animated: true, completion: nil)
+            
+            let vc = UIViewController()
+            vc.view.backgroundColor = .white
+            present(vc, animated: true, completion: nil)
+        }
+    }
 
 }
 
