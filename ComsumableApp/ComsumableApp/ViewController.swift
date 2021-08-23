0b7f3fa2e353d79842775ad8b230b73b344a5cd5
so yeon <alicesysy@swu.ac.kr>
2021-08-23 18:35:47 +0900

    [add] In App Purchases 프로젝트 추가

---
+++ ComsumableApp/ComsumableApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ComsumableApp
+//
+//  Created by soyeon on 2021/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
