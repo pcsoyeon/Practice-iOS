5f6bc79a941eb2085213de6877d9e06b1ab12e31
so yeon <alicesysy@swu.ac.kr>
2021-08-30 23:54:59 +0900

    [feat] ContentView UI 구현

--- StoreKit/StoreKit/ContentView.swift
+++ StoreKit/StoreKit/ContentView.swift
@@ -6,11 +6,38 @@
 //
 
 import SwiftUI
+import StoreKit
+
+// Fetch Products
+// Purchase Products
+// Update UI + Fetch Product State
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Image(systemName: "applelogo")
+                .resizable()
+                .frame(width: 70, height: 70)
+            
+            Text("Apple Store")
+                .bold()
+                .font(.system(size: 32))
+            
+            Image("appleWatch")
+                .resizable()
+                .aspectRatio(nil, contentMode: .fit)
+            
+            Button(action: {
+                
+            }) {
+                Text("Buy Now")
+                    .bold()
+                    .foregroundColor(.white)
+                    .frame(width: 220, height: 50)
+                    .background(Color.green)
+                    .cornerRadius(10)
+            }
+        }
     }
 }
 
