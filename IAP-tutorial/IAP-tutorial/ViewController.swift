99191ce4d375a58fed9dd2b36b313f83aab72308
so yeon <alicesysy@swu.ac.kr>
2021-08-31 00:36:40 +0900

    [feat] StoreKit config 구현

--- IAP-tutorial/IAP-tutorial/ViewController.swift
+++ IAP-tutorial/IAP-tutorial/ViewController.swift
@@ -6,14 +6,70 @@
 //
 
 import UIKit
+import StoreKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SKProductsRequestDelegate, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Test"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        // show purchase
+    }
+    
+    // Products
+    
+    enum Product: String, CaseIterable {
+        case removeAds = "com.myapp.removeAds"
+        case unlockEverything = "com.myapp.everything"
+        case getGems = "com.myapp.gems"
+    }
+    
+    private func fetchProducts() {
+        let request = SKProductsRequest(productIdentifiers: Set(Product.allCases.compactMap({
+            $0.rawValue
+        })))
+        request.delegate = self
+        request.start()
+    }
+    
+    func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+        DispatchQueue.main.async {
+            print("Count: \(response.products)")
+            self.models = response.products
+            self.tableView.reloadData()
+        }
+    }
+    
+    // MARK: - Properties
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    
+    // MARK: - Local Variables
+    
+    private var models = [SKProduct]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
     }
-
-
+    
 }
 
