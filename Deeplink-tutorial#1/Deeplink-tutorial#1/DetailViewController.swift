a654baa7a9f82c79ca3230fae4317fb4af56d90a
so yeon <alicesysy@swu.ac.kr>
2021-09-05 14:34:39 +0900

    [feat] deeplink 구현

---
+++ Deeplink-tutorial#1/Deeplink-tutorial#1/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  Deeplink-tutorial#1
+//
+//  Created by soyeon on 2021/09/05.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.title = "Detail"
+    }
+}
---
+++ Deeplink-tutorial#1/Deeplink-tutorial#1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Deeplink-tutorial#1
+//
+//  Created by soyeon on 2021/09/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "Main"
+    }
+
+
+}
+
